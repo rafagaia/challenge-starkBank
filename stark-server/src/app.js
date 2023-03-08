c9327620c17192acd80344f8c196852352b1dc2a
rafagaia <rafaelvsgaia@gmail.com>
2023-03-08 07:05:39 -0300

    fix: require morgan in app.js

--- stark-server/src/app.js
+++ stark-server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const morgan = require('morgan');
 const api = require('./routes/api')
 
 const app = express();
