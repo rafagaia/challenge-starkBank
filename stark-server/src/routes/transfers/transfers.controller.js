3dc113f7b07f36f054f5196bca350440854551d6
rafagaia <rafaelvsgaia@gmail.com>
2023-03-07 23:27:06 -0300

    chore: add log file to .gitignore, console.log in transfers hook

--- stark-server/src/routes/transfers/transfers.controller.js
+++ stark-server/src/routes/transfers/transfers.controller.js
@@ -2,6 +2,7 @@
 *   handles POST /webhook/transfers
 */
 async function hookTransfers(req, res) {
+    console.log("******Transfers Hook******");
     return res.status(200).json({"status": "OK - Transfers Event Webhook received."})
 }
 
